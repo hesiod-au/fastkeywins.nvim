c4d8911cb751e56e8c7cedd6df59b50df2121341
Hesiod <oneword1word@gmail.com>
2023-05-13 13:44:01 +1000

    debugging errors

--- lua/fastkeywins/init.lua
+++ lua/fastkeywins/init.lua
@@ -179,23 +179,24 @@ end
 
 M.toggle_minimize_window = function()
     local axes = return_window_axes()
-    if axes == "both" then
-        if _G.fkw_is_window_in_direction('j') then
-            vim.cmd('wincmd j')
-            if return_window_axes() == "both" then
-                vim.cmd('wincmd k')
-                resize_window("vertical")
-                return
-            else
-                vim.cmd('wincmd k')
-                resize_window("horizontal")
-                return
-            end
-        end
-    else
-        resize_window(axes)
-        return
-    end
+    return
+    -- if axes == "both" then
+    --     if _G.fkw_is_window_in_direction('j') then
+    --         vim.cmd('wincmd j')
+    --         if return_window_axes() == "both" then
+    --             vim.cmd('wincmd k')
+    --             resize_window("vertical")
+    --             return
+    --         else
+    --             vim.cmd('wincmd k')
+    --             resize_window("horizontal")
+    --             return
+    --         end
+    --     end
+    -- else
+    --     resize_window(axes)
+    --     return
+    -- end
 end
 
 
